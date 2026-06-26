66f5fd179c4aa98fd7b2d9b5105eb350e17b4593
Milat <112171950+Binary-Adventure@users.noreply.github.com>
2026-06-26 06:21:55 +0300

    Delete DataBase directory

--- DataBase/__init__.py
+++
@@ -1,4 +0,0 @@
-from .db_manager import DataBaseManager
-from .models import Client, CardType, Card, BonusAccount, Transaction, Receipt
-
-__all__ = ['DataBaseManager', 'Client', 'CardType', 'Card', 'BonusAccount', 'Transaction', 'Receipt']
